3e587d09d9533a25255daf1e2b3e2ab52b1382f5
doudou <doudou@123.com>
2015-06-25 21:48:41 +0800

    lol

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=liguoxian',
-    'username' => 'root',
+    'username' => 'dou',
     'password' => '317416',
     'charset' => 'utf8',
 ];
